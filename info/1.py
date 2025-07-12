d7f160d2e9c5e96e44a771eec0548007b224242f
blezz-tech <markus.jenya04@yandex.ru>
2025-07-12 12:19:34 +0300

    task 31,32,33,34

--- info/1.py
+++ info/1.py
@@ -441,19 +441,51 @@ def task_30():
 
 
 def task_31():
-    raise NotImplementedError("task_31 not implemented")
+    def f(x, y):
+        if x == y:
+            return 1
+        elif x > y or x == 14:
+            return 0
+        else:
+            return f(x + 1, y) + f(x + 2, y)
+    
+    return f(2, 9) * f(9, 18)
 
 
 def task_32():
-    raise NotImplementedError("task_32 not implemented")
+    def f(x, y):
+        if x == y:
+            return 1
+        elif x > y or x == 11 or x == 15:
+            return 0
+        else:
+            return f(x + 1, y) + f(x + 3, y) + f(x * 2, y)
+    
+    return f(2, 8) * f(8, 20)
 
 
 def task_33():
-    raise NotImplementedError("task_33 not implemented")
+    def f(x, y):
+        if x == y:
+            return 1
+        elif x > y or x == 9 or x == 15:
+            return 0
+        else:
+            return f(x + 1, y) + f(x + 3, y) + f(x * 3, y)
+    
+    return f(3, 18)
 
 
 def task_34():
-    raise NotImplementedError("task_34 not implemented")
+    def f(x, y):
+        if x == y:
+            return 1
+        elif x < y or x == 20:
+            return 0
+        else:
+            return f(x - 2, y) + f(x // 2, y)
+ 
+    return f(80, 40) * f(40, 1)
 
 
 def task_35():
