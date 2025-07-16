4f9b5dddf63f80a177cf1211b00611de4a61b521
blezz-tech <markus.jenya04@yandex.ru>
2025-07-16 17:11:47 +0300

    task 2

--- info/2.py
+++ info/2.py
@@ -11,7 +11,30 @@ def task_1():
 
 
 def task_2():
-    raise NotImplementedError("task_2 not implemented")    
+    #  A  B  C  D  E  F
+    # 10 11 12 13 14 15
+
+    for x in range(0, 46 + 1):
+        x1 = 1 * 47 ** 4 +\
+             x * 47 ** 3 +\
+             2 * 47 ** 2 +\
+             4 * 47 ** 1 +\
+            10 * 47 ** 0
+
+        x2 = x * 47 ** 4 +\
+             2 * 47 ** 3 +\
+             0 * 47 ** 2 +\
+             2 * 47 ** 1 +\
+             4 * 47 ** 0
+
+        x3 = 6 * 47 ** 3 +\
+             x * 47 ** 2 +\
+             0 * 47 ** 1 +\
+             8 * 47 ** 0
+        
+        r = x1 + x2 - x3
+        if r % 46 == 0:
+            return x
 
 
 def task_3():
